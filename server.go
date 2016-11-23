dc445b16cc1cc66b5eb7979fa046eb1103826488
mochi8k <y.hkh.185@gmail.com>
2016-11-23 11:01:35 +0900

    handle root

--- server.go
+++ server.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func HTTPHandler(w http.ResponseWriter, r *http.Request) {
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("/")
+
+	tpl, _ := template.ParseFiles("templates/attendance.tpl")
+	tpl.Execute(w, "")
+}
+
+func AttendanceHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("/attendance")
 
+	if r.Method != "POST" {
+		return
+	}
+
+	fmt.Println(r.Method)
 	fmt.Fprintf(w, "I've been working late every day this week.")
 }
 
 func main() {
 
-	http.HandleFunc("/attendance", HTTPHandler)
+	http.HandleFunc("/", RootHandler)
+	http.HandleFunc("/attendance", AttendanceHandler)
 
 	fmt.Println("activated the web server on port 8000")
 	http.ListenAndServe(":8000", nil)
