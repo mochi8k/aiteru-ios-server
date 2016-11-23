c2d1797cfa780a006954a9e4d16b56b2c6e29bcb
mochi8k <y.hkh.185@gmail.com>
2016-11-23 22:10:28 +0900

    handle mysql

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,15 +24,34 @@ func AttendanceHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(r.Method)
-	fmt.Fprintf(w, "I've been working late every day this week.")
+	r.ParseForm()
+
+	userName := strings.Join(r.Form["userName"], "")
+	currentTime := strings.Join(r.Form["currentTime"], "")
+	fmt.Println(userName)
+	fmt.Println(currentTime)
+
+	db, err := sql.Open("mysql", "root@/gosample")
+	errorChecker(err)
+
+	stmt, err := db.Prepare("insert attendance set username=?, time=?")
+	errorChecker(err)
+
+	res, err := stmt.Exec(userName, currentTime)
+	errorChecker(err)
+	fmt.Println(res)
 }
 
 func main() {
-
 	http.HandleFunc("/", RootHandler)
 	http.HandleFunc("/attendance", AttendanceHandler)
 
 	fmt.Println("activated the web server on port 8000")
 	http.ListenAndServe(":8000", nil)
+}
+
+func errorChecker(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
