9a6bd540f379dc794e56c82270d03cafad3b11c8
mochi8k <y.hkh.185@gmail.com>
2016-12-12 01:24:49 +0900

    grope sql

--- sql/places.sql
+++ sql/places.sql
@@ -26,3 +26,23 @@ create table place_status(
     `updated_at` datetime,
     `updated_by` int(11)
 );
+
+
+select
+p.id,
+p.place_name,
+group_concat(distinct u1.user_name) as owner_names,
+group_concat(distinct u2.user_name) as collaborator_names,
+u3.user_name as created_by,
+p.created_at,
+u4.user_name as updated_by,
+p.updated_at
+from places as p
+inner join place_owners as po on p.id = po.place_id
+inner join users as u1 on u1.id = po.owner_id
+inner join place_collaborators as pc on p.id = pc.place_id
+inner join users as u2 on u2.id = pc.collaborator_id
+inner join users as u3 on p.created_by=u3.id
+left outer join users as u4 on p.updated_by=u3.id
+group by p.id, u3.user_name, u4.user_name
+;
