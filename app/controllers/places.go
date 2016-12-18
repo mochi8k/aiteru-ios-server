3b665665b276c57d27f94725b58411fc770b0011
mochi8k <y.hkh.185@gmail.com>
2016-12-18 11:19:26 +0900

    use squirrel

--- app/controllers/places.go
+++ app/controllers/places.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	sq "github.com/Masterminds/squirrel"
 	_ "github.com/go-sql-driver/mysql"
 
 	"fmt"
@@ -17,49 +18,45 @@ func init() {
 	http.Handle("/places/", rest.APIResourceHandler(places{}))
 }
 
+func getDefaultSelectBuilder() sq.SelectBuilder {
+	return sq.
+		Select(`p.id,
+      p.place_name,
+      group_concat(distinct u1.id) as owner_ids,
+      group_concat(distinct u2.id) as collaborator_ids,
+      u3.id as created_by,
+      p.created_at,
+      u4.id as updated_by,
+      p.updated_at
+    `).
+		From("places as p").
+		Join("place_owners as po on p.id = po.place_id").
+		Join("users as u1 on u1.id = po.owner_id").
+		Join("place_collaborators as pc on p.id = pc.place_id").
+		Join("users as u2 on u2.id = pc.collaborator_id").
+		Join("users as u3 on p.created_by=u3.id").
+		LeftJoin("users as u4 on p.updated_by=u4.id").
+		GroupBy("p.id, u3.user_name, u4.user_name")
+}
+
 type places struct {
 	rest.APIResourceBase
 }
 
 func (p places) Get(url string, queries url.Values, body io.Reader) (rest.APIStatus, interface{}) {
-	fmt.Println("GET: /places")
-
-	fmt.Println(url)
-	fmt.Println(queries)
-	fmt.Println(body)
 
 	db, err := sql.Open("mysql", "root@/aiteru")
 	errorChecker(err)
 
 	defer db.Close()
 
-	res, err := db.Query(`
-		select
-		    p.id,
-		    p.place_name,
-		    group_concat(distinct u1.id) as owner_ids,
-		    group_concat(distinct u2.id) as collaborator_ids,
-		    u3.id as created_by,
-		    p.created_at,
-		    u4.id as updated_by,
-		    p.updated_at
-		from
-		    places as p
-		inner join
-		    place_owners as po on p.id = po.place_id
-		inner join
-		    users as u1 on u1.id = po.owner_id
-		inner join
-		    place_collaborators as pc on p.id = pc.place_id
-		inner join
-		    users as u2 on u2.id = pc.collaborator_id
-		inner join
-		    users as u3 on p.created_by=u3.id
-		left outer join
-		    users as u4 on p.updated_by=u4.id
-		group by
-		    p.id, u3.user_name, u4.user_name
-	`)
+	if id := url[len("/places/"):]; id != "" {
+		fmt.Println(id)
+	}
+
+	query, _, _ := getDefaultSelectBuilder().ToSql()
+	fmt.Println(query)
+	res, err := db.Query(query)
 
 	errorChecker(err)
 
