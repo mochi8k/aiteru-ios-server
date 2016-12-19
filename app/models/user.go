350354492ca6c652e48d4f4d137fb628de3ae1d8
mochi8k <y.hkh.185@gmail.com>
2016-12-19 22:43:36 +0900

    add user.go

---
+++ app/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	CreatedAt     string `json:"createdAt"`
+	CreatedUserID string `json:"createdUserId"`
+	UpdatedAt     string `json:"updatedAt"`
+	UpdatedUserID string `json:"updatedUserId"`
+}
