2f990ef5841922ee3882030c01404445d3945b72
mochi8k <y.hkh.185@gmail.com>
2016-12-21 22:18:07 +0900

    add log

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	_ "github.com/mochi8k/aiteru-ios-server/app/controllers"
 )
 
 func main() {
+	fmt.Println("activated the web server on port 8000")
 	http.ListenAndServe(":8000", nil)
 }
