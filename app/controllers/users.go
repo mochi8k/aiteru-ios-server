85c2f7d056bae2774038902b20159d9ac26f4b75
mochi8k <y.hkh.185@gmail.com>
2016-12-24 23:46:51 +0900

    users depends on httprouter

--- app/controllers/users.go
+++ app/controllers/users.go
@@ -1,11 +1,11 @@
 package controllers
 
 import (
-	_ "database/sql"
+	"database/sql"
 	_ "github.com/Masterminds/squirrel"
 	_ "github.com/go-sql-driver/mysql"
 
-	"fmt"
+	"github.com/julienschmidt/httprouter"
 	rest "github.com/mochi8k/aiteru-ios-server/app/http"
 	"github.com/mochi8k/aiteru-ios-server/app/models"
 	"io"
@@ -14,25 +14,27 @@ import (
 	_ "time"
 )
 
-type users struct {
-	rest.APIResourceBase
-}
-
 func init() {
-	http.Handle("/v1/users/", rest.APIResourceHandler(users{}))
+	rest.Register("/v1/users/", map[string]rest.Handler{
+		"GET": getUsers,
+	})
 }
 
-func (u users) Get(url string, queries url.Values, body io.Reader, session *models.Session) (rest.APIStatus, interface{}) {
-	fmt.Println("Get")
-	return rest.Success(http.StatusCreated), nil
+func getUsers(ps httprouter.Params, queries url.Values, body io.Reader, session *models.Session) (rest.APIStatus, interface{}) {
+	db, err := sql.Open("mysql", "root@/aiteru")
+	errorChecker(err)
+
+	defer db.Close()
+
+	return rest.Success(http.StatusOK), nil
 }
 
-func (u users) Post(url string, queries url.Values, body io.Reader, session *models.Session) (rest.APIStatus, interface{}) {
-	fmt.Println("Post")
-	// sq.
-	// 	Insert("users").
-	// 	Columns("user_name", "created_at", "created_by").
-	// 	Values()
+// func (u users) Post(url string, queries url.Values, body io.Reader, session *models.Session) (rest.APIStatus, interface{}) {
+// 	fmt.Println("Post")
+// 	// sq.
+// 	// 	Insert("users").
+// 	// 	Columns("user_name", "created_at", "created_by").
+// 	// 	Values()
 
-	return rest.Success(http.StatusCreated), nil
-}
+// 	return rest.Success(http.StatusCreated), nil
+// }
