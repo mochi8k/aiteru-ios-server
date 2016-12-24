0fafd728a0fb472f5c045e5fa80a5f36bfaea1b5
mochi8k <y.hkh.185@gmail.com>
2016-12-24 22:57:26 +0900

    server depends on httprouter

--- server.go
+++ server.go
@@ -5,9 +5,10 @@ import (
 	"net/http"
 
 	_ "github.com/mochi8k/aiteru-ios-server/app/controllers"
+	"github.com/mochi8k/aiteru-ios-server/app/handlers/router"
 )
 
 func main() {
 	fmt.Println("activated the web server on port 8000")
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", router.GetInstance())
 }
