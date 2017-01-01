8add1b3affe67df95f4fca1f02cf4208ca4a3e11
mochi8k <y.hkh.185@gmail.com>
2017-01-01 21:05:03 +0900

    add status as parameter

--- app/models/place.go
+++ app/models/place.go
@@ -1,12 +1,13 @@
 package models
 
 type Place struct {
-	ID              string   `json:"id"`
-	Name            string   `json:"name"`
-	OwnerIDs        []string `json:"ownerIds"`
-	CollaboratorIDs []string `json:"collaboratorIds"`
-	CreatedAt       string   `json:"createdAt"`
-	CreatedUserID   string   `json:"createdUserId"`
-	UpdatedAt       string   `json:"updatedAt"`
-	UpdatedUserID   string   `json:"updatedUserId"`
+	ID              string       `json:"id"`
+	Name            string       `json:"name"`
+	OwnerIDs        []string     `json:"ownerIds"`
+	CollaboratorIDs []string     `json:"collaboratorIds"`
+	CreatedAt       string       `json:"createdAt"`
+	CreatedUserID   string       `json:"createdUserId"`
+	UpdatedAt       string       `json:"updatedAt"`
+	UpdatedUserID   string       `json:"updatedUserId"`
+	Status          *PlaceStatus `json:"status"`
 }
