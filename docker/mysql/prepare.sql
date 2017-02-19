feda28456f68e56dd40cc9f5f74f94d65851c23b
mochi8k <y.hkh.185@gmail.com>
2017-02-19 14:29:26 +0900

    add Dockerfile of MySQL

---
+++ docker/mysql/prepare.sql
@@ -0,0 +1,43 @@
+-- places
+create table places(
+    `id` int(11) unsigned not null auto_increment,
+    `place_name` varchar(255) not null,
+    `created_at` datetime,
+    `created_by` int(11),
+    `updated_at` datetime,
+    `updated_by` int(11),
+    primary key(id)
+);
+
+create table place_owners(
+    `place_id` int(11) unsigned not null,
+    `owner_id` int(11) unsigned not null,
+    primary key(place_id, owner_id)
+);
+
+create table place_collaborators(
+    `place_id` int(11) unsigned not null,
+    `collaborator_id` int(11) unsigned not null,
+    primary key(place_id, collaborator_id)
+);
+
+create table place_status(
+    `place_id` int(11) unsigned not null,
+    `is_open` boolean not null,
+    `updated_at` datetime,
+    `updated_by` int(11)
+);
+
+-- users
+create table users(
+    `id` int(11) unsigned not null auto_increment,
+    `user_name` varchar(255) not null,
+    `created_at` datetime,
+    `created_by` int(11),
+    `updated_at` datetime,
+    `updated_by` int(11),
+    primary key(id)
+);
+
+insert into users (user_name, created_at) values ("admin", current_time);
+       
